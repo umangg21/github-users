39eb9b83fec46cb43cf7e3e95a59fb7e990d9bad
umangg21 <umangg21@gmail.com>
2019-01-14 23:27:10 +0530

    Updates

---
+++ precache-manifest.b0f98c9cf3817d2f09aa1b349770f407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de0a1a1c3d3bc0675305",
+    "url": "/github-users/static/js/runtime~main.de0a1a1c.js"
+  },
+  {
+    "revision": "c9c5354c8dbd522e6f26",
+    "url": "/github-users/static/js/main.c9c5354c.chunk.js"
+  },
+  {
+    "revision": "6480c7373d8cbe23ab51",
+    "url": "/github-users/static/js/1.6480c737.chunk.js"
+  },
+  {
+    "revision": "c9c5354c8dbd522e6f26",
+    "url": "/github-users/static/css/main.4ebada87.chunk.css"
+  },
+  {
+    "revision": "8a5cd187354ac0cbc790da174d080d6b",
+    "url": "/github-users/index.html"
+  }
+];
